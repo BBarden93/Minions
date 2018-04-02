4302e93c8e0014eb3ddb481877147099dc9ef08b
Brittany Barden <bbarden93@yahoo.com>
2018-04-02 16:19:02 -0700

    handle edit and delete btns

--- client/src/httpClient.js
+++ client/src/httpClient.js
@@ -13,4 +13,12 @@ httpClient.createMinion = function(fields){
     return this({method: 'post', url: '/api/minions', data: fields})
 }
 
+httpClient.updateMinion = function(id, fields){
+    return this({method: 'patch', url: `/api/minions/${id}`, data: fields})
+}
+
+httpClient.deleteMinion = function(id) {
+    return this({method: 'delete', url: `/api/minions/${id}`})
+}
+
 export default httpClient
--- client/src/views/MinionDetail.js
+++ client/src/views/MinionDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import httpClient from '../httpClient.js'
-import {Button, Modal, ModalBody, ModalHeader, ModalFooter} from 'reactstrap'
+import {Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, ModalFooter} from 'reactstrap'
 
 class MinionDetail extends React.Component {
     
@@ -14,6 +14,25 @@ class MinionDetail extends React.Component {
             modalOpen: true 
         })
     }
+    handleDeleteClick() {
+        httpClient.deleteMinion(this.props.match.params.id).then((serverResponse) => {
+            this.props.history.push('/minions')
+        })
+    }
+    handleEditFormSubmit(evt) {
+        evt.preventDefault()
+        const {name, imageURL} = this.refs
+        const minionFormFields = {
+            name: name.refs.name.value,
+            imageURL: imageURL.refs.imageURL.value
+        }
+        httpClient.updateMinion(this.props.match.params.id, minionFormFields).then((serverResponse) => {
+            this.setState({
+                modalOpen: false,
+                minion: serverResponse.data.minion 
+            })
+        })
+    }
     
     componentDidMount() {
         httpClient.getAMinion(this.props.match.params.id).then((serverResponse) => {
@@ -24,7 +43,7 @@ class MinionDetail extends React.Component {
     }
 
     render(){
-        const {minion} = this.state
+        const {minion, modalOpen} = this.state
         console.log(minion)
         if(!minion) return <h1>Loading Minion...</h1>
         return (
@@ -35,8 +54,24 @@ class MinionDetail extends React.Component {
                     <Button onClick={this.handleEditClick.bind(this)} color="primary">Edit</Button>
                 </div>
 
-                <Modal isOpen={this.state.modalOpen}>
-
+                <Modal isOpen={modalOpen}>
+                    <ModalHeader>Edit Minion</ModalHeader>
+                    <Form onSubmit={this.handleEditFormSubmit.bind(this)}>
+                        <ModalBody>
+                            <FormGroup>
+                                <Label for="name">name</Label>
+                                <Input defaultValue={minion.name} ref="name" innerRef="name" type="text" id="id" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="imageURL">Image URL</Label>
+                                <Input defaultValue={minion.imageURL} ref="imageURL" innerRef="imageURL" type="text" id="imageURL" />
+                            </FormGroup>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button type="submit" color="info">Update</Button>
+                            <Button type="button" onClick={this.handleDeleteClick.bind(this)}color="danger">Delete</Button>
+                        </ModalFooter>
+                    </Form>
                 </Modal>
 
             </div>
